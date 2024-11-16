a89eab57d630fc94379facd7a67998886f1e81bb
Yudhishthra Sugumaran <66850474+0xYudhishthra@users.noreply.github.com>
2024-11-16 09:42:18 +0700

    Refactor CTFFactory and FPMMFactory contracts

--- src/CTFFactory.sol
+++ src/CTFFactory.sol
@@ -1,12 +1,18 @@
-// SPDX-License-Identifier: UNLICENSED
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.13;
 
-import {IERC20} from "openzeppelin-contracts/contracts/token/ERC20/IERC20.sol";
+import {IERC20} from "../lib/openzeppelin-contracts/contracts/token/ERC20/IERC20.sol";
 import {ConditionalTokens} from "./ConditionalTokens.sol";
 import {FixedProductMarketMaker} from "./FPMM.sol";
 
+library MarketMakerLib {
+    function validateInputs(address collateral, uint256 fee) internal pure {
+        require(collateral != address(0), "Invalid collateral token");
+        require(fee <= 10 ** 18, "Fee must be <= 100%");
+    }
+}
+
 contract FPMMFactory {
-    // Events
     event FPMMCreated(
         address indexed creator,
         address indexed fpmm,
@@ -17,72 +23,78 @@ contract FPMMFactory {
         uint256 fee
     );
 
-    // State variables
-    // ConditionalTokens public conditionalTokens;
+    function createFPMM(
+        IERC20 collateralToken,
+        ConditionalTokens conditionalTokens,
+        bytes32 conditionId,
+        uint256 outcomeSlotCount,
+        uint256 fee
+    ) external returns (FixedProductMarketMaker) {
+        MarketMakerLib.validateInputs(address(collateralToken), fee);
+
+        bytes32[] memory conditionIds = new bytes32[](1);
+        conditionIds[0] = conditionId;
+
+        uint256[] memory outcomeSlotCounts = new uint256[](1);
+        outcomeSlotCounts[0] = outcomeSlotCount;
+
+        FixedProductMarketMaker fpmm = new FixedProductMarketMaker(
+            conditionalTokens,
+            collateralToken,
+            conditionIds,
+            outcomeSlotCounts,
+            fee
+        );
+
+        emit FPMMCreated(
+            tx.origin,
+            address(fpmm),
+            address(conditionalTokens),
+            address(collateralToken),
+            conditionIds,
+            outcomeSlotCounts,
+            fee
+        );
+
+        return fpmm;
+    }
+}
+
+contract CTFFactory {
     mapping(address => bool) public isFPMM;
-    uint256 public fpmmCount;
+    FPMMFactory public immutable fpmmFactory;
 
     constructor() {
-      
+        fpmmFactory = new FPMMFactory();
     }
 
     function createFPMM(
-    IERC20 collateralToken,
-    address oracle,
-    bytes32 questionId,
-    uint256 outcomeSlotCount,
-    uint256 endTime,
-    uint256 fee
-) external returns (FixedProductMarketMaker) {
-    require(address(collateralToken) != address(0), "Invalid collateral token");
-    require(fee <= 10 ** 18, "Fee must be <= 100%");
+        IERC20 collateralToken,
+        address oracle,
+        bytes32 questionId,
+        uint256 outcomeSlotCount,
+        uint256 endTime,
+        uint256 fee
+    ) external returns (FixedProductMarketMaker) {
+        ConditionalTokens conditionalTokens = new ConditionalTokens();
 
-    // Deploy ConditionalTokens first
-    ConditionalTokens conditionalTokens = new ConditionalTokens();
+        bytes32 conditionId = conditionalTokens.prepareCondition(
+            oracle,
+            questionId,
+            outcomeSlotCount,
+            endTime
+        );
 
-    // Prepare condition
-    bytes32 conditionId = conditionalTokens.prepareCondition(
-        oracle,
-        questionId,
-        outcomeSlotCount,
-        endTime
-    );
+        FixedProductMarketMaker fpmm = fpmmFactory.createFPMM(
+            collateralToken,
+            conditionalTokens,
+            conditionId,
+            outcomeSlotCount,
+            fee
+        );
 
-    // Setup arrays for FPMM constructor
-    bytes32[] memory conditionIds = new bytes32[](1);
-    conditionIds[0] = conditionId;
-
-    uint256[] memory outcomeSlotCounts = new uint256[](1);
-    outcomeSlotCounts[0] = outcomeSlotCount;
-
-    // Create FPMM with the new ConditionalTokens
-    FixedProductMarketMaker fpmm = new FixedProductMarketMaker(
-        conditionalTokens,
-        collateralToken,
-        conditionIds,
-        outcomeSlotCounts,
-        fee
-    );
-
-    // Register FPMM
-    isFPMM[address(fpmm)] = true;
-    fpmmCount++;
-
-    emit FPMMCreated(
-        msg.sender,
-        address(fpmm),
-        address(conditionalTokens),
-        address(collateralToken),
-        conditionIds,
-        outcomeSlotCounts,
-        fee
-    );
-
-    return fpmm;
-}
-
-    function getFPMMCount() external view returns (uint256) {
-        return fpmmCount;
+        isFPMM[address(fpmm)] = true;
+        return fpmm;
     }
 
     function isValidFPMM(address fpmmAddress) external view returns (bool) {
